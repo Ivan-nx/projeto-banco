4d01fb5057b900aceb1a02d00471a74681495087
Ivan Reis <ivanreis83@gmail.com>
2021-09-28 15:29:44 -0300

    Corregindo a paralvra gerente

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {Cliente} from "./Cliente.js";
+import { Cliente } from "./Cliente.js";
 import { Gerente } from "./Funcionarios/Gerente.js";
 import { Diretor } from "./Funcionarios/Diretor.js";
 import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
@@ -7,8 +7,8 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 diretor.cadastrarSenha("963963");
 
-const genrete = new Gerente("Ricardo",  5000, 12345678901);
-genrete.cadastrarSenha("963");
+const gerente = new Gerente("Ricardo", 5000, 12345678901);
+gerente.cadastrarSenha("963");
 
 const cliente = new Cliente("Lais", 78945612302, "456")
 const diretoEstaLogado = SistemaAutenticacao.login(diretor, "963963");
